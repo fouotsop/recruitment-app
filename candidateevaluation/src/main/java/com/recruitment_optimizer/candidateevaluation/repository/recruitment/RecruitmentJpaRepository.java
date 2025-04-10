392fd2bb7dd18cd2b719bbc08d6a641855316914
fouotsop <patrick.fouotsop@facsciences-uy1.cm>
2025-04-10 14:07:18 +0100

    added endpoint to fetch recruitment along with its criteria by id

--- candidateevaluation/src/main/java/com/recruitment_optimizer/candidateevaluation/repository/recruitment/RecruitmentJpaRepository.java
+++ candidateevaluation/src/main/java/com/recruitment_optimizer/candidateevaluation/repository/recruitment/RecruitmentJpaRepository.java
@@ -1,9 +1,14 @@
 package com.recruitment_optimizer.candidateevaluation.repository.recruitment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.recruitment_optimizer.candidateevaluation.model.Recruitment;
 
 public interface RecruitmentJpaRepository extends JpaRepository<Recruitment, String> {
+
+
+    @Query(value = "SELECT r FROM Recruitment r LEFT JOIN FETCH r.recruitmentCriteria WHERE r.id = ?1")
+    Recruitment fetchById(String id);
     
 }
